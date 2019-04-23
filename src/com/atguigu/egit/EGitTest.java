bdcb1f61533eebb0672de7b896f739c48d0dce1d
Blakeluj <757104196@qq.com>
2019-04-23 20:07:32 +0800

    改错

--- src/com/atguigu/egit/EGitTest.java
+++ src/com/atguigu/egit/EGitTest.java
@@ -4,6 +4,6 @@ public class EGitTest {
 
 	public static void main(String[] args) {
 		System.out.println("Hello World!");
-		Sysout.out.println("Hello EGit!")
+		System.out.println("Hello EGit!");
 	}
 }
