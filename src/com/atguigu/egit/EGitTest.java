0444ff3baa1b4638f6ae44459f435b0c380b1bec
Blakeluj <757104196@qq.com>
2019-04-23 20:40:36 +0800

    蓝廋

--- src/com/atguigu/egit/EGitTest.java
+++ src/com/atguigu/egit/EGitTest.java
@@ -5,5 +5,6 @@ public class EGitTest {
 	public static void main(String[] args) {
 		System.out.println("Hello World!");
 		System.out.println("This is a good day!");
+		System.out.println("简直了！");
 	}
 }
